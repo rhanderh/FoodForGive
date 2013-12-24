73c59350ae186defa298dabffd1082f8243e61a6
rhanderh <rhanderh@gmail.com>
2013-12-23 19:16:34 -0800

    frontend design working minus forms

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ FoodForGive::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'confessions#new'
+   root :to => 'confessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
