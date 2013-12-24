7c4748277d4ff0c547848ffbc70fa870ff1cd34a
rhanderh <rhanderh@gmail.com>
2013-12-23 19:32:38 -0800

    set serve static assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ config.action_mailer.default_url_options = {
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
