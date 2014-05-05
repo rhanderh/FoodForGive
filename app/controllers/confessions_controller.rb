85b3947bd96f719974ed6dd1fd0dc2be7416f2ec
Ryan Handerhan <rhanderh@gmail.com>
2014-05-04 23:00:38 -0700

    re-enable sniff on homepage once more

--- app/controllers/confessions_controller.rb
+++ app/controllers/confessions_controller.rb
@@ -1,5 +1,5 @@
 class ConfessionsController < ApplicationController
-  #before_filter :check_for_mobile
+  before_filter :check_for_mobile
   before_action :set_confession, only: [:show, :edit, :update, :destroy]
   before_action :check_if_ipayed, only: [:index]
   respond_to :html, :js, :json
